cbaa7f199ddd1b9ac34f5f86b5733dd21b26cb90
ntapire <digantarajbaral@gmail.com>
2025-09-24 19:32:51 +0545

    Basic setup for the project is completed.This includes installation of necessary modules, connect to mongo db and creating a simple route

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const moongose = require("mongoose");
 const port = 3000;
-mongoose.connect('mongodb://127.0.0.1:27017/wanderLust');
+
+async function main() {
+   await moongose.connect('mongodb://127.0.0.1:27017/wanderLust');
+}
 
 app.listen(port,()=>{
     console.log("Server is listening on port",port);
