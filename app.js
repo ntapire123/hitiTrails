a8ca8f39010d4a303563924f873be1040e2acb1f
ntapire <digantarajbaral@gmail.com>
2025-09-25 09:18:27 +0545

    This commit was done after modifying the schema and adding listing route and its corresponding templates

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const app = express();
 const mongoose = require("mongoose");
 const port = 3000;
 const listing = require("./models/listing.js");
+const path = require("path");
 
 
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
 
 
 async function main() {
@@ -15,14 +20,28 @@ app.listen(port,()=>{
     console.log("Server is listening on port",port);
 })
 
-app.get("/",(req,res)=>{
-    res.send("Hi I am root")
-})
 
 main().then((result)=>{
     console.log("Connection success");
 }).catch((err)=>{
- console.log("some error happened")
+    console.log("some error happened")
+})
+
+app.get("/",(req,res)=>{
+    res.send("Hi I am root")
 })
 
 
+
+
+app.get("/listings",async (req,res)=>{
+const allListings = await listing.find({});
+res.render("listings/index.ejs",{allListings});
+})
+
+
+app.get("/listings/:id",async (req,res)=>{
+    let { id } = req.params;
+    const listingbyid =  await listing.findById(id)
+    res.render("listings/moreInfo.ejs",{ listingbyid });
+})
