e5ddcc4964a7b391c9746883d5cb786399899714
yantpire <digantarajbaral@gmail.com>
2026-02-11 00:08:57 +0545

    Fix: Handle undefined error in wrapAsync utility to prevent TypeError

--- utils/wrapAsync.js
+++ utils/wrapAsync.js
@@ -1,5 +1,11 @@
 module.exports = (fn) => {
-    return (req, res, next) => {
-        fn(req, res, next).catch(next);
-    };
+  return (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch((err) => {
+      // Check if err exists and has properties before accessing them
+      if (err && typeof err === 'object' && Object.keys(err).length > 0) {
+        return next(err);
+      }
+      return next(err);
+    });
+  };
 };
